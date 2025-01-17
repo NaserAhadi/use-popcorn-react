b3fdbb0da8428e476479ec179f56b94ed7193f3a
Naser Ahadi <n.ahadi@vandar.io>
2025-01-17 19:49:21 +0330

    create reusable StarRating component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+import StarRating from "./StarRating";
+import { useState } from "react";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <span>the movie is rated {movieRating} scores</span>
+    </div>
+  );
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/*<App />*/}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={5} color="red" size={24} defaultRating={3} />
+    <Test />
+  </React.StrictMode>,
 );
